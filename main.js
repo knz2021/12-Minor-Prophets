74ae85fb354900abd8ece4fff74cf4d66d8b61a3
KNZ <86813245+knz2021@users.noreply.github.com>
2022-10-25 16:36:56 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+function clickDisplay1() {
+  const element = document.getElementById("text-button1");
+  element.innerHTML = "１，ホセア";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay2() {
+  const element = document.getElementById("text-button2");
+  element.innerHTML = "２，ヨエル";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay3() {
+  const element = document.getElementById("text-button3");
+  element.innerHTML = "３，アモス";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay4() {
+  const element = document.getElementById("text-button4");
+  element.innerHTML = "４，オバデヤ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay5() {
+  const element = document.getElementById("text-button5");
+  element.innerHTML = "５，ヨナ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay6() {
+  const element = document.getElementById("text-button6");
+  element.innerHTML = "６，ミカ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay7() {
+  const element = document.getElementById("text-button7");
+  element.innerHTML = "７，ナホム";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay8() {
+  const element = document.getElementById("text-button8");
+  element.innerHTML = "８，ハバクク";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay9() {
+  const element = document.getElementById("text-button9");
+  element.innerHTML = "９，ゼパニヤ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay10() {
+  const element = document.getElementById("text-button10");
+  element.innerHTML = "１０，ハガイ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay11() {
+  const element = document.getElementById("text-button11");
+  element.innerHTML = "１１，ゼカリヤ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay12() {
+  const element = document.getElementById("text-button12");
+  element.innerHTML = "１２、マラキ";
+  element.style.backgroundColor = "pink";
+}
